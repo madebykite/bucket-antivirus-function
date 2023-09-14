77e2eebe7419994cdef3f1088cec6ae5a987b4e1
Matthew Glubb <matthew.glubb@madebykite.com>
2023-09-14 12:33:07 +0100

    BBCWRS-357 Removed redundant metrics call

--- scan.py
+++ scan.py
@@ -257,9 +257,6 @@ def lambda_handler(event, context):
             result_time,
         )
 
-    metrics.send(
-        env=ENV, bucket=s3_object.bucket_name, key=s3_object.key, status=scan_result
-    )
     # Delete downloaded file to free up room on re-usable lambda function container
     try:
         os.remove(file_path)
